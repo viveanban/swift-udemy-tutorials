8380edb77bb2bc115183535f94ff265690d6cbec
Viveka Anban <viveka98@hotmail.ca>
2020-05-25 12:53:50 -0400

    Calculator layout completed

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by ANBAN, Viveka on 2020-05-25.
+//  Copyright © 2020 ANBAN, Viveka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
